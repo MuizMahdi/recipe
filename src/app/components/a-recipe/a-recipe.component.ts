3564864a4ad6a42b37a641fda5f429f6845ba1c9
Muiz Mahdi <moezmahdy@gmail.com>
2019-03-27 15:54:12 +0300

    [Refactor] renamed variables and added comments in a-recipe component

--- src/app/components/a-recipe/a-recipe.component.ts
+++ src/app/components/a-recipe/a-recipe.component.ts
@@ -1,91 +1,108 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from './../../models/Recipe';
 
+
 @Component({
-  selector: 'app-a-recipe',
-  templateUrl: './a-recipe.component.html',
-  styleUrls: ['./a-recipe.component.css']
+   selector: 'app-a-recipe',
+   templateUrl: './a-recipe.component.html',
+   styleUrls: ['./a-recipe.component.css']
 })
 
 
 export class ARecipeComponent implements OnInit 
 { 
+   @Output() selectedRecipe = new EventEmitter<Recipe>();
+   @Input('recipe') recipe:Recipe;
 
-//---------------------------------------------------------------------------------------------------------------------------------//
-  @Output() theRecipeSelected2 = new EventEmitter<Recipe>();
-  @Input('recipeVal2') recipe2: Recipe;
+   showMoreText: boolean = false; 
+   shortText: boolean;
 
-  showMoreText: boolean = false; 
-  shortText: boolean;
+   fullDescription: string;
+   miniDescription: string;
+   imageSource: string;
 
-  fullDescription: string;
-  miniDescription: string;
-  imageSource: string;
-//---------------------------------------------------------------------------------------------------------------------------------//
 
-  constructor() { }
+   constructor() 
+   { }
 
-  ngOnInit() 
-  { 
-    this.setDefaultImageOnError();
-  }
 
-  ngAfterContentInit()
-  {
-    this.initShowMore();
-  }
+   ngOnInit() 
+   { 
+      this.setDefaultImageOnError();
+   }
 
-  initShowMore()
-  {
-    if(this.recipe2.description.length > 90) 
-    {
-      this.miniDescription = this.recipe2.description.slice(0,90) + "..."; //start sliced
-      this.shortText = false; //view the "ShowMore"
-    } 
-    else 
-    { 
-      this.miniDescription = this.recipe2.description; 
-      this.shortText = true; //dont view the "ShowMore"
-    }
-    
-    this.fullDescription = this.recipe2.description;
-    this.recipe2.description = this.miniDescription;
-  }
 
-  setDefaultImageOnError()
-  { 
-    if(this.recipe2.imagesrc.length > 5)
-    {
-      this.imageSource = this.recipe2.imagesrc;
-    }
-    else
-    {
-      this.imageSource = "https://cdn3.iconfinder.com/data/icons/food-and-ingredients/512/Food_and_Drinks_Fish_dish-01-512.png"
-      this.recipe2.imagesrc = this.imageSource;
-    }
-  }
+   ngAfterContentInit()
+   {
+      // Set the length of recipe description text to be viewed before hiding it and adding 'ShowMore' 
+      this.initShowMore();
+   }
 
-  getSelected2()
-  {
-    this.theRecipeSelected2.emit(this.recipe2);
-  }
 
-  showMore()
-  {
-    
-    this.showMoreText = !this.showMoreText;
+   initShowMore()
+   {
+      // If the description is more than 90 characters long
+      if(this.recipe.description.length > 90) 
+      {
+         // Construct minified description of 90 characters
+         this.miniDescription = this.recipe.description.slice(0,90) + "..."; //start sliced
+         // View the "ShowMore"
+         this.shortText = false; 
+      } 
+      else 
+      {
+         // No limited description if its 90 characters or fewer
+         this.miniDescription = this.recipe.description;
+         // Dont view the "ShowMore" 
+         this.shortText = true; 
+      }
+      
+      this.fullDescription = this.recipe.description;
+      this.recipe.description = this.miniDescription;
+   }
 
-    if(this.showMoreText) 
-    {
-      this.recipe2.description = this.fullDescription;
-      this.shortText = true; // hide the 'show more' after viewing full description (until page is left)
-    } 
-    
-    if(!this.showMoreText)
-    {
-      this.recipe2.description = this.miniDescription;
-    }
 
-  }
+   setDefaultImageOnError()
+   { 
+      // Validate recipe image source
+      if(this.recipe.imagesrc.length > 5) 
+      {
+         this.imageSource = this.recipe.imagesrc;
+      }
+      else 
+      {
+         // Use a template recipe image in case an invalid image source is posted
+         this.imageSource = "https://cdn3.iconfinder.com/data/icons/food-and-ingredients/512/Food_and_Drinks_Fish_dish-01-512.png"
+         this.recipe.imagesrc = this.imageSource;
+      }
+   }
 
+
+   setSelected()
+   {
+      // Set current recipe as selected recipe and emit it to recipes component 
+      this.selectedRecipe.emit(this.recipe);
+   }
+
+
+   showMore()
+   {
+      // Toggle showing full description
+      this.showMoreText = !this.showMoreText;
+
+      // When showing full text
+      if(this.showMoreText)
+      {
+         // Set the recipe's description as the full description
+         this.recipe.description = this.fullDescription;
+
+         // hide the 'show more' after viewing full description
+         this.shortText = true;
+      }
+      else
+      {  // When hiding full text
+         // Set the recipe's description as the minified short description
+         this.recipe.description = this.miniDescription;
+      }
+   }
 }
